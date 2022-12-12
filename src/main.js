fa3ab1fce61e8eee5c184a1bae1b8625f76c99f0
jkaushal947 <47825941+jkaushal947@users.noreply.github.com>
2022-12-12 21:30:23 +0000

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import Phaser from './lib/phaser.js'
 import Start from './scenes/Start.js'
 import Game from './scenes/Game.js'
 import GameOver from './scenes/GameOver.js'
+import Win from './scenes/Win.js'
 
 export default new Phaser.Game({
     type: Phaser.AUTO,
