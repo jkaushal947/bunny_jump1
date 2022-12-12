545be715a4bbb35b4f02bf700acbda469d3e3cd2
jkaushal947 <47825941+jkaushal947@users.noreply.github.com>
2022-12-12 21:33:09 +0000

    Add files via upload

--- src/scenes/Win.js
+++ src/scenes/Win.js
@@ -1,6 +1,6 @@
 import Phaser from '../lib/phaser.js'
 
-export default class GameOver extends Phaser.Scene {
+export default class Win extends Phaser.Scene {
     heightPoint
     carrotCollected
     text
